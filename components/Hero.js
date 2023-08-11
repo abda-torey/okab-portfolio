32c39372b3beef83c037d14b47e3b1c90f0113e6
Okashe <okashabdi88@gmail.com>
2023-08-10 19:35:27 -0500

    action btn color

--- components/Hero.js
+++ components/Hero.js
@@ -43,7 +43,7 @@ Join us Today
 Empowering businesses through tailored web design solutions. We set them for unprecedented growth.
 </p>
 <div class="flex flex-wrap items-center justify-center">
-<button class="inline-flex items-center px-4 py-2 my-4 font-semibold text-black bg-transparent border border-white rounded-3xl hover:bg-orange-900 hover:text-white">
+<button class="inline-flex items-center px-4 py-2 my-4 font-semibold text-white bg-orange-900 border border-white rounded-3xl hover:bg-orange-500 hover:text-white">
 <span>Find out more</span>
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 ml-3 bi bi-arrow-right" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
